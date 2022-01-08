cb277e0b5ac136ab361262553d08ce52ad8eb6c4
RiceDaddy321 <0carlitos2@gmail.com>
2022-01-08 03:54:01 -0600

    deleted a dublicate htaccess and modified app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var http = require('http');
 const path = require("path");
 const createError = require("http-errors");
 
@@ -21,8 +22,17 @@ app.set('view engine', 'ejs');
 //set default path
 app.use(express.static(path.join(__dirname, 'public')));
 
-//create the port where the server will mount to
-const PORT = process.env.PORT || 3000;
+/**
+ * Get port from environment and store in Express.
+ */
+ var port = normalizePort(process.env.PORT || '3000');
+ app.set('port', port);
+
+ /**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
 
 //use routers
 app.use("/", homeRouter);
@@ -41,6 +51,69 @@ app.get("/test", (req,res) => {
     res.redirect('/');
 });
 
-app.listen(PORT, () => {
-    console.log('Node app is running on port', app.get('port'));
-});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+ function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+  
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+  
+  function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+  }
