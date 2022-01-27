c516272ace5716883c8d97bbb543631fdad31346
RiceDaddy321 <0carlitos2@gmail.com>
2022-01-27 02:18:08 -0600

    Used ejs tags to make partials for the header, footer, and head, making my code maintainable.

--- routes/home.js
+++ routes/home.js
@@ -3,25 +3,21 @@ var router = express.Router();
 
 router.get(['/', '/home'], function (req, res) {
     res.render('homePage');
-    console.log('Home paged accessed by: TK'); //debugging
 });
 
 router.get('/about', function(req, res)
 {
     res.render('./header/aboutPage');
-    console.log('About page accessed by: TK');
 });
 
 router.get('/projects', function(req, res)
 {
     res.render('./header/projectsPage');
-    console.log('projects page has been accessed');
 });
 
 router.get('/contacts', function(req, res)
 {
     res.render('./header/contactsPage');
-    console.log('contacts page has been accessed');
 });
 
 //send the router
