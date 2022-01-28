4b0a0beb536df3e388fce013df5d469a239a62bf
RiceDaddy321 <0carlitos2@gmail.com>
2022-01-28 04:55:20 -0600

    Created error page for The Dog API project, added information to the about page, and added changelog to home page.

--- routes/dogAPI.js
+++ routes/dogAPI.js
@@ -14,6 +14,8 @@ router.get('/random', function(req, res) {
   axios.get("https://api.thedogapi.com/v1/images/search")
   .then(function (response) {
     res.render('./dogapi/dogRandom', {data : JSON.stringify(response.data), raw : response.data});
+  }).catch(function (err) {
+    res.render('./dogapi/dogError', {error: err});
   });
 });
 
@@ -22,6 +24,9 @@ router.get('/independent', function(req, res) {
   axios.get("https://api.thedogapi.com/v1/images/search")
   .then(function (response) {
     res.render('./dogapi/dogIndependent', {data : JSON.stringify(response.data), raw : response.data});
+  }).catch(function (err) {
+    res.render('./dogapi/dogError', {error: err});
+    
   });
 });
 
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,45 @@
 const express = require('express');
+const axios = require('axios')
 var router = express.Router();
 
+//returns a json
+async function getCommits(){
+    return await axios.get("https://api.github.com/repos/ricedaddy321/deploy-portfolio-app/commits")
+    .then(function(response) {
+        //we assume that everything works fine
+        let author = [];
+        let message = [];
+        let date =[];
+        let sha = [];
+        let link = [];
+        //the reponse is an array so we need to iterate through each
+        for (const [index, element] of response.data.entries()) {
+            author.push(element.commit.author.name);
+            message.push(element.commit.message);
+            date.push(element.commit.author.date);
+            sha.push(element.sha);
+            link.push(element.html_url);
+        };
+        
+        var json_data = {"author": author
+        , "message": message
+        , "date": date
+        , "sha": sha 
+        , "link": link};
+        console.log(json_data.sha);
+        return JSON.stringify(json_data);
+    });
+}
+
 router.get(['/', '/home'], function (req, res) {
-    res.render('homePage');
+    //get all of the json_data
+    getCommits().then(function(json_data_response) {
+        
+        res.render('homePage', JSON.parse(json_data_response));
+    }).catch(function(err) {
+        res.render('homePage');
+    });
+    
 });
 
 router.get('/about', function(req, res)
@@ -19,6 +56,7 @@ router.get('/contacts', function(req, res)
 {
     res.render('./header/contactsPage');
 });
+
 
 //send the router
 module.exports = router;
